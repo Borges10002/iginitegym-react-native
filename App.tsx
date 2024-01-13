4cd106df6103f78f056379fa990169849d11f8c0
Borges10002 <borges10002@gmail.com>
2024-01-13 19:22:48 -0400

    class about native base

--- App.tsx
+++ App.tsx
@@ -1,25 +1,23 @@
 import { Text, View, StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import { Loading } from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <NativeBaseProvider>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? (
-        <Text>Open up App.tsx to start working on your app!</Text>
-      ) : (
-        <View />
-      )}
-    </View>
+      {fontsLoaded ? <Loading /> : <Loading />}
+    </NativeBaseProvider>
   );
 }
--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,26 @@
-plugins: [
-  [
-    'module-resolver',
-    {
-      root: ['./src'],
-      alias: {
-        '@dtos': './src/dtos',
-        '@assets': './src/assets',
-        '@components': './src/components',
-        '@screens': './src/screens',
-        '@storage': './src/storage',
-        '@utils': './src/utils',
-        '@services': './src/services',
-        '@hooks': './src/hooks',
-        '@contexts': './src/contexts',
-        '@routes': './src/routes'
-      }
-    },
-  ],
-],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@dtos": "./src/dtos",
+            "@assets": "./src/assets",
+            "@components": "./src/components",
+            "@screens": "./src/screens",
+            "@storage": "./src/storage",
+            "@utils": "./src/utils",
+            "@services": "./src/services",
+            "@hooks": "./src/hooks",
+            "@contexts": "./src/contexts",
+            "@routes": "./src/routes",
+          },
+        },
+      ],
+    ],
+  };
+};
