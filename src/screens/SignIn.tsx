9720403a81e6de00c8c72086ad22ab41192711a5
Borges10002 <borges10002@gmail.com>
2024-01-19 21:48:03 -0400

    class about inputs

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -2,10 +2,11 @@ import { Center, Heading, Image, Text, VStack } from "native-base";
 
 import BackgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg";
+import { Input } from "@components/Input";
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="gray.700">
+    <VStack flex={1} bg="gray.700" px={10}>
       <Image
         source={BackgroundImg}
         alt="Pessoas treinando"
@@ -21,9 +22,16 @@ export function SignIn() {
       </Center>
 
       <Center>
-        <Heading color="gray.100" fontSize="xl" fontFamily="heading">
+        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
           Acessa sua conta
         </Heading>
+
+        <Input
+          placeholder="E-mail"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <Input placeholder="Senha" secureTextEntry />
       </Center>
     </VStack>
   );
