cb33ad6764b6a05420dfa5b87e5b628e3498707e
Borges10002 <borges10002@gmail.com>
2024-01-20 19:12:44 -0400

    class about of componente button

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -3,6 +3,7 @@ import { Center, Heading, Image, Text, VStack } from "native-base";
 import BackgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg";
 import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 export function SignIn() {
   return (
@@ -32,6 +33,8 @@ export function SignIn() {
           autoCapitalize="none"
         />
         <Input placeholder="Senha" secureTextEntry />
+
+        <Button title="Acessar" />
       </Center>
     </VStack>
   );
