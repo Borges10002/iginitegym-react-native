c5d656e5e89306f4836fd8454b0cca1b1044c54a
Borges10002 <borges10002@gmail.com>
2024-02-16 22:04:03 -0400

    button lagout

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,16 @@
 import { HStack, Heading, Text, VStack } from "native-base";
+import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        source={{ uri: "https://avatars.githubusercontent.com/u/32522338?v=4" }}
+        alt="Imagem do usuário"
+        size={16}
+        mr={4}
+      />
+
       <VStack>
         <Text color="gray.100">Olá,</Text>
 
