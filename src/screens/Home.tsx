81e5098cba9ae5e68cb8876f7daa0edc9a77cdcb
Borges10002 <borges10002@gmail.com>
2024-02-21 21:43:36 -0400

    update

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import { FlatList, VStack } from "native-base";
+import { FlatList, HStack, Heading, VStack, Text } from "native-base";
 import { useState } from "react";
 
 import { Group } from "@components/Group";
@@ -33,6 +33,17 @@ export function Home() {
         my={10}
         maxH={10}
       />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercicios
+          </Heading>
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   );
 }
