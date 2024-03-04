da0f4c174cce405bf9aa7e1fb6663e9787a926f7
Borges10002 <borges10002@gmail.com>
2024-03-03 21:25:27 -0400

    class about feito skeleton

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,19 +1,34 @@
-import { ScrollView, VStack, Center } from "native-base";
+import { Center, ScrollView, Skeleton, VStack } from "native-base";
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
+import { useState } from "react";
+
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [phtoIsLoading, setPhotoIsLoading] = useState(false);
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
       <ScrollView>
         <Center mt={6} px={10}>
-          <UserPhoto
-            source={{ uri: "https://github.com/Borges10002.png" }}
-            alt="Foto do usuário"
-            size={33}
-          />
+          {phtoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: "https://github.com/Borges10002.png" }}
+              alt="Foto do usuário"
+              size={PHOTO_SIZE}
+            />
+          )}
         </Center>
       </ScrollView>
     </VStack>
