441ba740bf0ae880f510d3744c1e233dd6f02938
Borges10002 <borges10002@gmail.com>
2024-03-09 22:30:56 -0400

    update

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,18 @@
-import { Center, ScrollView, Skeleton, VStack, Text } from "native-base";
+import {
+  Center,
+  ScrollView,
+  Skeleton,
+  VStack,
+  Text,
+  Heading,
+} from "native-base";
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
 import { useState } from "react";
 import { TouchableOpacity } from "react-native";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 const PHOTO_SIZE = 33;
 
@@ -13,7 +22,7 @@ export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
-      <ScrollView>
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt={6} px={10}>
           {phtoIsLoading ? (
             <Skeleton
@@ -42,7 +51,33 @@ export function Profile() {
               Alterar foto
             </Text>
           </TouchableOpacity>
+
+          <Input bg="gray.600" placeholder="Nome" />
+
+          <Input bg="gray.600" placeholder="E-mail" isDisabled />
         </Center>
+
+        <VStack px={10} mt={12} mb={9}>
+          <Heading
+            color="gray.200"
+            fontSize="md"
+            mb={2}
+            alignSelf="flex-start"
+            mt={12}
+          >
+            Alterar senha
+          </Heading>
+
+          <Input bg="gray.600" placeholder="Senha antiga" secureTextEntry />
+          <Input bg="gray.600" placeholder="Nova senha" secureTextEntry />
+          <Input
+            bg="gray.600"
+            placeholder="Confirme a nova senha"
+            secureTextEntry
+          />
+
+          <Button title="Atualizar" mb={4} />
+        </VStack>
       </ScrollView>
     </VStack>
   );
