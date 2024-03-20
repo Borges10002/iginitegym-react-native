82109ea6c5757ec17518e83541b6573a3c38aa5f
Borges10002 <borges10002@gmail.com>
2024-03-19 21:38:06 -0400

    update

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -21,12 +21,16 @@ export function Profile() {
   const [phtoIsLoading, setPhotoIsLoading] = useState(false);
 
   async function handleUserProtoSelect() {
-    await ImagePicker.launchImageLibraryAsync({
+    const photoSelected = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       quality: 1,
       aspect: [4, 4],
       allowsEditing: true,
     });
+
+    if (photoSelected.canceled) {
+      return;
+    }
   }
 
   return (
