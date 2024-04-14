88ac10496fcc8958cc75a1a80b08b4beb0227c2d
Borges10002 <borges10002@gmail.com>
2024-04-14 19:10:54 -0400

    unificando o create e provider do contexto

--- iginitegym/App.tsx
+++ iginitegym/App.tsx
@@ -1,17 +1,18 @@
-import { StatusBar } from "react-native";
-import { NativeBaseProvider } from "native-base";
 import {
-  useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
+  useFonts,
 } from "@expo-google-fonts/roboto";
+import { NativeBaseProvider } from "native-base";
+import { StatusBar } from "react-native";
 
 import { Routes } from "@routes/index";
 
 import { THEME } from "./src/theme";
 
 import { Loading } from "@components/Loading";
-import { AuthContext } from "@contexts/AuthContext";
+
+import { AuthContextProvider } from "@contexts/AuthContext";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -23,9 +24,9 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <AuthContext.Provider value={{}}>
+      <AuthContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
-      </AuthContext.Provider>
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
--- iginitegym/src/contexts/AuthContext.tsx
+++ iginitegym/src/contexts/AuthContext.tsx
@@ -1,10 +1,31 @@
 import { UserDTO } from "@dtos/UserDTO";
-import { createContext } from "react";
+import { ReactNode, createContext } from "react";
 
 export type AuthContextDataProps = {
   user: UserDTO;
 };
 
+type AuthContextProviderProps = {
+  children: ReactNode;
+};
+
 export const AuthContext = createContext<AuthContextDataProps>(
   {} as AuthContextDataProps
 );
+
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  return (
+    <AuthContext.Provider
+      value={{
+        user: {
+          id: "1",
+          name: "diego da silva borges",
+          email: "borges10002@gmail.com",
+          avatar: "https://github.com/Borges10002.png",
+        },
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
