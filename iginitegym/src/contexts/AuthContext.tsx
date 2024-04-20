62302b4d33e3f06e64fb98df985bbf962ca8a39a
Borges10002 <borges10002@gmail.com>
2024-04-19 22:15:19 -0400

    update

--- iginitegym/src/contexts/AuthContext.tsx
+++ iginitegym/src/contexts/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { UserDTO } from "@dtos/UserDTO";
 import { api } from "@services/api";
-import { ReactNode, createContext, useState } from "react";
+import { storageUserGet, storageUserSave } from "@storage/storageConfig";
+import { ReactNode, createContext, useEffect, useState } from "react";
 
 export type AuthContextDataProps = {
   user: UserDTO;
@@ -24,11 +25,24 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
       if (data.user) {
         setUser(data.user);
+        storageUserSave(data.user);
       }
     } catch (error) {
       throw error;
     }
   }
+
+  async function loadUserData() {
+    const userLogged = await storageUserGet();
+
+    if (userLogged) {
+      setUser(userLogged);
+    }
+  }
+
+  useEffect(() => {
+    loadUserData();
+  });
 
   return (
     <AuthContext.Provider
