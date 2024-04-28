b2f75a19230971932ad0c922cbd8d847812568c6
Borges10002 <borges10002@gmail.com>
2024-04-28 10:05:00 -0400

    updatre

--- iginitegym/src/contexts/AuthContext.tsx
+++ iginitegym/src/contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { UserDTO } from "@dtos/UserDTO";
 import { api } from "@services/api";
+import { storageAuthTokenSave } from "@storage/storageAuthToken";
 import {
   storageUserGet,
   storageUserRemove,
@@ -27,13 +28,29 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [isLoadingUserStorageData, setIsLoadingUserStorageData] =
     useState(true);
 
+  async function storageUserAndToken(userData: UserDTO, token: string) {
+    try {
+      setIsLoadingUserStorageData(true);
+
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+      await storageUserSave(userData);
+      await storageAuthTokenSave(token);
+
+      setUser(userData);
+    } catch (error) {
+      throw error;
+    } finally {
+      setIsLoadingUserStorageData(false);
+    }
+  }
+
   async function signIn(email: string, password: string) {
     try {
       const { data } = await api.post("/sessions", { email, password });
 
       if (data.user) {
-        setUser(data.user);
-        storageUserSave(data.user);
+        storageUserAndToken(data.user, data.token);
       }
     } catch (error) {
       throw error;
