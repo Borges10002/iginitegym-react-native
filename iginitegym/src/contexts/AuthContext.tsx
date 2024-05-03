040ff6bed699e991b080e754adbab4a2869080ae
Borges10002 <borges10002@gmail.com>
2024-05-02 21:03:58 -0400

    update

--- iginitegym/src/contexts/AuthContext.tsx
+++ iginitegym/src/contexts/AuthContext.tsx
@@ -2,6 +2,7 @@ import { UserDTO } from "@dtos/UserDTO";
 import { api } from "@services/api";
 import {
   storageAuthTokenGet,
+  storageAuthTokenRemove,
   storageAuthTokenSave,
 } from "@storage/storageAuthToken";
 import {
@@ -74,6 +75,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       setUser({} as UserDTO);
 
       await storageUserRemove();
+      await storageAuthTokenRemove();
     } catch (error) {
       throw error;
     } finally {
